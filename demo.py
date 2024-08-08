4b6c28bf58f26468481db9f555092d7f3865ad75
ColdBeer <mtornos@gmail.com>
2024-08-08 20:33:49 +0200

    20240808-20:32-config con debug

---
+++ demo.py
@@ -0,0 +1,19 @@
+TRANSICIONES = {
+            'Secuencia': {
+                    'Paso1': ['Paso2',
+                            ['Paso1_Tip1',
+                            'Paso1_Tip2', 
+                            'Paso1_Tip3']
+                                ],
+                    'Paso2': ['Paso3',
+                        ['Paso2_Tip1',
+                        'Paso2_Tip2', 
+                        'Paso2_Tip3']
+                                ],
+                    'Paso3': ['Paso4',
+                        ['Paso3_Tip1', 
+                        'Paso3_Tip2', 
+                        'Paso3_Tip3']
+                                ]
+                        } 
+                }
