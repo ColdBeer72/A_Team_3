7bbf7de8f80732b6242860da0ca48554b72d52e9
Juan Crescenti <joancres12@gmail.com>
2024-08-10 20:28:07 +0200

    20240810-20:27-Markdowns_update

--- hojas/01_Presentation.py
+++ hojas/01_Presentation.py
@@ -6,33 +6,37 @@ st.markdown(HIDE_IMG_FS, unsafe_allow_html=True)
 # Título principal
 col1, col2 = st.columns([3, 1])
 with col1:
-    st.image(PAGE_LOGO, use_column_width="auto")
+    st.image(
+        PAGE_LOGO,
+        use_column_width="auto"
+    )
 with col2:
-    st.image(HAB_LOGO_PATH, width=100)
-
+    st.image(
+        HAB_LOGO_PATH,
+        width=100
+    )
 # Presentacion del Proyecto
 st.header("Presentación del Proyecto", anchor = False, divider="red")
 presentacion_path = "streamlit_sources/page1/presentation.md"
 presentacion = read_markdown_file(presentacion_path)
 st.markdown(presentacion)
-with st.expander("Tecnologías Utilizadas:"):
+with st.expander("**Tecnologías Utilizadas:**"):
     techs_md_path = "streamlit_sources/page1/techs.md"
     techs_md = read_markdown_file(techs_md_path)
     st.markdown(techs_md)
-st.markdown("AI + ML + UXUI:")
-with st.expander("AI - Inteligencia Artificial"):
+st.markdown("#### AI + ML + UXUI:")
+with st.expander("**AI - Inteligencia Artificial**"):
     ia_md_path = "streamlit_sources/page1/ia.md"
     ia_md = read_markdown_file(ia_md_path)
     st.markdown(ia_md)
-with st.expander("ML - Machine Learning"):
+with st.expander("**ML - Machine Learning**"):
     ml_md_path = "streamlit_sources/page1/ml.md"
     ml_md = read_markdown_file(ml_md_path)
     st.markdown(ml_md)
-with st.expander("UX/UI - Experiencia de Usuario / Interfaz de Usuario"):
+with st.expander("**UX/UI - Experiencia de Usuario / Interfaz de Usuario**"):
     uxui_md_path = "streamlit_sources/page1/uxui.md"
     uxui_md = read_markdown_file(uxui_md_path)
     st.markdown(uxui_md)
-
 
 # Firma
 st.subheader("Proyecto de:", anchor = False, divider="gray")
