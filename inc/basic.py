6bd6b0d39bfc1f7acd7f2c02ff44befc2fb5991e
Juan Crescenti <joancres12@gmail.com>
2024-08-13 13:18:14 +0200

    20240813-13:18-bottom_padding

--- inc/basic.py
+++ inc/basic.py
@@ -15,7 +15,7 @@ button[title="View fullscreen"]{
 <style>
                .block-container {
                     padding-top: 2rem;
-                    padding-bottom: 0rem;
+                    padding-bottom: 1rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
                 }
