f7b628d7a12cf98303f1fbcd823f28b861193b8e
ColdBeer <mtornos@gmail.com>
2024-08-14 12:51:05 +0200

    20240814-12:50-Cambios Presentacion

--- hojas/01_Presentation.py
+++ hojas/01_Presentation.py
@@ -24,23 +24,46 @@ with st.expander("**Tecnologías Utilizadas:**"):
     techs_md_path = "streamlit_sources/page1/techs.md"
     techs_md = read_markdown_file(techs_md_path)
     st.markdown(techs_md)
-st.markdown("#### AI + ML + UXUI:")
-col1, col2, col3 = st.columns(3)
-with col1:
-    with st.expander("**AI - Inteligencia Artificial**"):
-        ia_md_path = "streamlit_sources/page1/ia.md"
-        ia_md = read_markdown_file(ia_md_path)
-        st.markdown(ia_md)
-with col2:
-    with st.expander("**ML - Machine Learning**"):
-        ml_md_path = "streamlit_sources/page1/ml.md"
-        ml_md = read_markdown_file(ml_md_path)
-        st.markdown(ml_md)
-with col3:
-    with st.expander("**UX/UI - Experiencia de Usuario / Interfaz de Usuario**"):
-        uxui_md_path = "streamlit_sources/page1/uxui.md"
-        uxui_md = read_markdown_file(uxui_md_path)
-        st.markdown(uxui_md)
+_, columna, _ = st.columns([1,3,1], gap ="small", vertical_alignment = "top")
+columna.markdown("#### AI + ML + UXUI + CODING:")
+tab1, tab2, tab3, tab4 = columna.tabs([":bulb: **AI - Inteligencia Artificial**",
+                                 ":robot_face: **ML - Machine Learning**",
+                                 ":soap: **UX/UI - Experiencia de Usuario / Interfaz de Usuario**",
+                                 ":1234: **Codificación del Proyecto**"])
+
+with tab1:
+    ia_md_path = "streamlit_sources/page1/ia.md"
+    ia_md = read_markdown_file(ia_md_path)
+    st.markdown(ia_md)
+with tab2:
+    ml_md_path = "streamlit_sources/page1/ml.md"
+    ml_md = read_markdown_file(ml_md_path)
+    st.markdown(ml_md)
+with tab3:
+    coding_md_path = "streamlit_sources/page1/uxui.md"
+    coding_md = read_markdown_file(coding_md_path)
+    st.markdown(coding_md)
+with tab4:
+    coding_md_path = "streamlit_sources/page1/coding.md"
+    coding_md = read_markdown_file(coding_md_path)
+    st.markdown(coding_md)
+
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     with st.expander("**AI - Inteligencia Artificial**"):
+#         ia_md_path = "streamlit_sources/page1/ia.md"
+#         ia_md = read_markdown_file(ia_md_path)
+#         st.markdown(ia_md)
+# with col2:
+#     with st.expander("**ML - Machine Learning**"):
+#         ml_md_path = "streamlit_sources/page1/ml.md"
+#         ml_md = read_markdown_file(ml_md_path)
+#         st.markdown(ml_md)
+# with col3:
+#     with st.expander("**UX/UI - Experiencia de Usuario / Interfaz de Usuario**"):
+#         coding_md_path = "streamlit_sources/page1/uxui.md"
+#         coding_md = read_markdown_file(coding_md_path)
+#         st.markdown(coding_md)
 
 # Firma
 st.subheader("Proyecto de:", anchor = False, divider="gray")
--- streamlit_sources/page1/coding.md
+++ streamlit_sources/page1/coding.md
@@ -6,7 +6,7 @@ Ha sido **objetivo número uno** el mantener una organización y una codificaci�
 
 - **hojas**: Ahí están los ficheros Python para Streamlit que definen las páginas del proyecto.
 - **inc**: En este lugar definimos las **librerías propias** en Python que utilizamos en el proyecto:
-    - **basic** es la librería que define configuraciones y funciones básicas relacionadas con la interfaz de usuario o páginas de streamlit
+    - **basic** es la librería que define funciones básicas relacionadas con la interfaz de usuario o páginas de streamlit
     - **config** es el fichero donde definimos variables globales, para su fácil configuración
     - otros, librerías de clases y funciones específicas para manejo de vídeo e IA
 - **streamlit_sources**: Aquí disponemos todo el material de apoyo (tanto gráfico como de texto/markdown) que utilizamos en las páginas, de forma que limitamos al máximo el uso de *"textos"* en el código. Internamente, se subidivide en carpetas en función de la página a la que afecta.
