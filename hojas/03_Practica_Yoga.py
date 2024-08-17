f80b72d5cf0f3d858fa582454e5d67ede0a6aea7
Juan Crescenti <joancres12@gmail.com>
2024-08-17 12:19:34 +0200

    fuera_ft_webcam

--- hojas/03_Practica_Yoga.py
+++ hojas/03_Practica_Yoga.py
@@ -107,10 +107,43 @@ with down_col2:
         media_stream_constraints=media_stream_constraints,
         async_processing=True
     )
-    down_col2_webcam(
-        webrtc_ctx,
-        user_pose,
-        up_col3_progress_bar,
-        up_col4_status,
-    )
+    while webrtc_ctx.state.playing:
+        print("En marcha!")
+        st.session_state.grabando = True
+        print(st.session_state.grabando)
+        keypoints = keypoint_queue.get()
+        print(keypoints)
+        if st.session_state.frame_count % 10 == 0:
+            frame_counter_increment()
+            estado_usuario = check_postura(
+                user_pose,
+                keypoints
+            )
+            print(f"Estado: {estado_usuario}")
+            if estado_usuario:
+                st.session_state.frames_success += FRAMES_SUCCESS_RATIO
+                up_col3_update_progress_bar(up_col3_progress_bar)
+                if st.session_state.frames_success == 100:
+                    print("Exito")
+                    pose_success(
+                        user_pose,
+                        up_col4_status,
+                    )
+            else:
+                print("U TRIED")
+                up_col3_update_progress_bar(up_col3_progress_bar)
+                up_col4_update_status(up_col4_status, False)
+                reset_frame_success()
+        else:
+            print("here?")
+            frame_counter_increment()
+    else:
+        keypoint_queue.empty()
+        st.session_state.grabando = False
+    # down_col2_webcam(
+    #     webrtc_ctx,
+    #     user_pose,
+    #     up_col3_progress_bar,
+    #     up_col4_status,
+    # )
 
